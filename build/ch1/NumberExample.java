87ebd427aa1808918264407af924239605409d91
Christopher Tse <christopher-tse@outlook.com>
2017-03-27 13:41:23 -0500

    Build 2017/03/27 with numbers question

---
+++ build/ch1/NumberExample.java
@@ -0,0 +1,13 @@
+public class NumberExample {
+	public static void main(String[] args) {
+		
+		// declare an integer variable myInt with a value of 20
+		int myInt = 20;
+		// declare a floating point variable myFloat with a value of 3.14
+		double myFloat = 3.14;
+
+		// Print out each of the variables
+		System.out.println(myInt);
+		System.out.println(myFloat);
+	}
+}

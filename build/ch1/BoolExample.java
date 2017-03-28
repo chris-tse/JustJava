27c81cb102950e3712bc36958060dedc27bf0cd0
Christopher Tse <christopher-tse@outlook.com>
2017-03-28 00:22:54 -0500

    Add code examples to boolean page

---
+++ build/ch1/BoolExample.java
@@ -0,0 +1,7 @@
+public class BoolExample {
+	public static void main(String[] args) {
+		boolean isHungry = true;
+		// isHungry was true, but we are not NOT hungry, so we invert with the ! symbol
+		System.out.println("I am not hungry: " + !isHungry);
+	}
+}

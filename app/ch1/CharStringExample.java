2fe637bb3b2f247c6f3471e748b461fc4ee4d511
Christopher Tse <christopher-tse@outlook.com>
2017-03-27 23:21:49 -0500

    Added comments to explain concatenation

--- app/ch1/CharStringExample.java
+++ app/ch1/CharStringExample.java
@@ -4,7 +4,7 @@ public class CharStringExample {
 		String myName = "John Doe";
 		// chars only use single quotes
 		char myFaveLetter = 'X';
-
+		// Strings and chars can be concatenated using the + symbol
 		System.out.println("Hello, my name is " + myName + " and my favorite letter is " + myFaveLetter);
 	}
 }

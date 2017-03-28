a584d862af7e64e5e8ac3247647da3615cd840db
Christopher Tse <christopher-tse@outlook.com>
2017-03-27 23:18:42 -0500

    Add example to built letters.html

---
+++ build/ch1/CharStringExample.java
@@ -0,0 +1,10 @@
+public class CharStringExample {
+	public static void main(String[] args) {
+		// Strings are declared using double quotes
+		String myName = "John Doe";
+		// chars only use single quotes
+		char myFaveLetter = 'X';
+
+		System.out.println("Hello, my name is " + myName + " and my favorite letter is " + myFaveLetter);
+	}
+}

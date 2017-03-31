a90cf2415d4cc9f3870e530d0875292960d86aa2
Christopher Tse <christopher-tse@outlook.com>
2017-03-31 02:45:41 -0500

    Build with andor done

---
+++ build/ch2/andortable.java
@@ -0,0 +1,6 @@
+x        y          x && y     x || y
+----------------------------------------
+true     true       true       true
+true     false      false      true
+false    true       false      true
+false    false      false      false

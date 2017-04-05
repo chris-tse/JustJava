a89213e69dd4d6743ce8f932502ae78024f1cd37
Christopher Tse <christopher-tse@outlook.com>
2017-04-05 11:19:58 -0500

    Add if else example

---
+++ app/ch2/IfElseExample.java
@@ -0,0 +1,12 @@
+public class IfElseExample {
+	public static void main(String[] args) {
+		int age = 22;
+
+		// Exact same functionality as before
+		if (age >= 21) {
+			System.out.println("Would you like some beer?");
+		} else {
+			System.out.println("Have some milk, kiddo");
+		}
+	}
+}

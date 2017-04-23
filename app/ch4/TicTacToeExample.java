f3403a2bbb40e26d076d1eff5507a9441f049fcd
Christopher Tse <christopher-tse@outlook.com>
2017-04-23 17:33:09 -0500

    Minor edit to example

--- app/ch4/TicTacToeExample.java
+++ app/ch4/TicTacToeExample.java
@@ -1,14 +1,14 @@
 public class TicTacToeExample {
 	public static void main(String[] args) {
 		// Assign a value to the possible states of a square
-		int blank = 0;
-		int x     = 1;
-		int o     = 2;
+		int BLANK = 0;
+		int X     = 1;
+		int O     = 2;
 
 		// Declare 2-D Array
-		int[][] board = {{o, blank,   x  },
-						 {x,   x  ,   o  },
-						 {o, blank, blank}};
+		int[][] board = {{O, BLANK,   X  },
+						 {X,   X  ,   O  },
+						 {O, BLANK, BLANK}};
 
 		// Print out each element in the array
 		for (int i = 0; i < 3; i++) {

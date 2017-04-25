ed847f6afe60f40a49e1bb77389da5fe64bb9d04
Christopher Tse <christopher-tse@outlook.com>
2017-04-25 12:40:20 -0500

    Minor edits

--- app/ch4/TicTacToeExample.java
+++ app/ch4/TicTacToeExample.java
@@ -13,7 +13,7 @@ public class TicTacToeExample {
 		// Print out each element in the array
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				System.out.print(board[i][j] + " ");
+				System.out.print( board[i][j] + " " );
 			}
 			System.out.println();
 		}
